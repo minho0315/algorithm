fa8800708c151f78ae4b6d5d04541b1a42087d7f
repl.it user <replituser@example.com>
2021-02-28 15:32:52 +0000

    codeupbasic100 to 20

---
+++ codeUp/basic100.py
@@ -0,0 +1,79 @@
+# # 001
+# print("Hello")
+
+# # 002
+# print("Hello World")
+
+# # 003
+# print("Hello")
+# print("World")
+
+# # 004
+# print("'Hello'")
+
+# # 005
+# print('"Hello World"')
+
+# # 006
+# print("\"!@#$%^&*()\'")
+
+# # 007
+# print('"C:\\Download\\\'hello\'.py"')
+
+# # 008
+# print("print(\"Hello\\nWorld\")")
+
+# # 009
+# c = input()
+# print(c)
+
+# # 010
+# n = input()
+# print(n)
+
+# # 011
+# f = input()
+# print(f)
+
+# # 012
+# a = input()
+# b = input()
+# print(a)
+# print(b)
+
+# # 013
+# a = input()
+# b = input()
+# print(b)
+# print(a)
+
+# # 014
+# a = input()
+# print(a)
+# print(a)
+# print(a)
+
+# # 015
+# a,b = input().split()
+# print(a)
+# print(b)
+
+# # 016
+# c1, c2 = input().split()
+# print(c2, c1)
+
+# # 017
+# s = input()
+# print(s,s,s)
+
+# # 018
+# a,b = input().split(':')
+# print(a,b,sep=':')
+
+# # 019
+# y, m, d = input().split('.')
+# print(d,m,y,sep='-')
+
+# # 020
+# a,b = input().split('-')
+# print(a,b,sep='')
