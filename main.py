59d3a9a6fca0250038c22733f64d78cdb89b1414
repl.it user <replituser@example.com>
2021-03-02 06:01:41 +0000

    codeup100 to 96

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# 071
+# # 096
+# d=[]
+# for i in range(20):
+#   a =[]
+#   a = input().split()
+  
+
+# for i in range(1,20):
+#   for j in range(1,20):
+#     print(d[i][j], end=' ')
+#   print()
